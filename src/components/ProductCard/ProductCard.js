3b5d5b4039600cd6002a773993d6f1995d2f7b55
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-09-11 23:14:42 +0300

    Add basic styles

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,20 +1,26 @@
 import React from 'react';
-
+import parfume from '../../assets/img/image-product-desktop.jpg';
 import styles from './ProductCard.module.scss';
 
 function ProductCard() {
   return (
     <main>
-      <h2>PERFUME</h2>
-      <h1>Gabrielle Essence Eau De Parfum</h1>
-      <p>
-        A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator
-        for the House of CHANEL.
-      </p>
-      <p>
-        $149.99 <span>$169.99</span>
-      </p>
-      <button>Add to Cart</button>
+      <div className={styles.parfumeImage}>
+        <img src={parfume} alt="parfume" />
+      </div>
+
+      <section>
+        <h2>PERFUME</h2>
+        <h1>Gabrielle Essence Eau De Parfum</h1>
+        <p>
+          A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator
+          for the House of CHANEL.
+        </p>
+        <p>
+          $149.99 <span>$169.99</span>
+        </p>
+        <button>Add to Cart</button>
+      </section>
     </main>
   );
 }
